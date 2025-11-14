fa0c0057bdb74a7930aefdd0b3ce852d1af59374
mk7x7 <mikxelsilvax777@gmail.com>
2025-11-14 00:56:38 -0300

    common: add ViperFX

--- mt6768.mk
+++ mt6768.mk
@@ -424,3 +424,6 @@ PRODUCT_COPY_FILES += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/mt6768-common/mt6768-common-vendor.mk)
+
+# ViperFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
