c1f3697bb1a8912a2820642bf4b88fc5b11ebbcd
mk7x7 <mikxelsilvax777@gmail.com>
2025-12-02 16:36:24 -0300

    Remove some packages from build

--- RemoveApps/Android.mk
+++ RemoveApps/Android.mk
@@ -5,7 +5,10 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-
+    Seedvault \
+    Jelly \
+    Music \
+    Recorder
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
